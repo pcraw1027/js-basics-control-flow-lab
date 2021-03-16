df09dc0d8af2af7cef64f4d67264ab7da33364a6
petercrawford <pcraw1027@gmail.com>
2021-03-15 21:37:49 -0400

    commit tree 2e6c51441f355355954311f9f14bb894482020e2

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(trip){
   // Write your code here!
+  let tripMessage = ""
+//  tripGladly = 
+  if (trip <= 400) {
+    tripMessage = "This one is on me!"
+  } else if (trip > 2500) {
+    tripMessage = "No can do."
+  } else if (trip > 2000) {
+    tripMessage = "I will gladly take your thirty bucks."
+  }
+  return tripMessage
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let tripMessage = ""
+  if (city === "NYC") {
+    tripMessage = "Ok, sounds good."
+  } else {
+    tripMessage = "No go."
+  }
+  return tripMessage
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let tipMessage = ""
+  if (tip === "generous") {
+    tipMessage = "Thank you so much."
+  } else if (tip === "not as generous"){
+    tipMessage = "Thank you."
+  } else if (tip ==="thanks for everything") {
+    tipMessage = "Bye."
+  }
+  return tipMessage
 }
